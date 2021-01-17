b730068265024f1fa9b8ee9dfe499ae02ed724e3
SamChambers <samj.chambers95@gmail.com>
2021-01-17 17:45:32 -0500

    add config

--- nsd.scheduler.py
+++ nsd.scheduler.py
@@ -1,24 +1,48 @@
+#!/usr/bin/env python3
 
+# Imports
+import argparse
+import configparser
 import email
 import imaplib
+import os
+import pytz
 import re
 import sys
-from datetime import datetime
-import pytz
-
 import time
 
+# From Import
 from icalendar import Calendar, Event
+from datetime import datetime
 
-
+# Email Imports
 import smtplib
-from pathlib import Path
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
 from email.utils import COMMASPACE, formatdate
 from email import encoders
-import tempfile
+
+
+class Shift():
+
+    def __init__(self):
+        self.day = None
+        self.start_date_time_obj = None
+        self.end_date_time_obj = None
+        self.role = None
+    # End of def __init__()
+# End of clas shift()
+
+
+def move_email(mail, mail_id, location):
+    result = mail.copy(mail_id, location)
+    
+    if result[0] == 'OK':
+        mail.store(mail_id, '+FLAGS', '\\Deleted')
+        mail.expunge()
+
+# End of move_email()
 
 
 def send_mail(send_from, send_to, subject, message, files=[],
@@ -63,14 +87,155 @@ def send_mail(send_from, send_to, subject, message, files=[],
     smtp.quit()
 
 
+def iphone_fix(mail_content):
 
+    # Handle Iphone forwarding issues
+
+    fixed_lines = ""
     
+    for line in mail_content.split('\n'):
+        if len(line) == 0:
+            continue
+        # End of if len()
+        if line == ">=20\r":
+            continue
+        # End of if len()
+        if line[:2] == "> ":
+            line = line[2:]
+        # End of if len()
+        if line[-2:] == "=\r":
+            fixed_lines+= line[:-2]
+            continue
+        # End of if len()
+        
+        fixed_lines += line
+        fixed_lines += '\n'
 
-EMAIL = 'nsd.scheduler@gmail.com'
-PASSWORD = 'Blue42Blue42'
-SERVER = 'imap.gmail.com'
+    # End of for line in mail_content
+
+    return fixed_lines
+
+# End of def iphone_fix()
 
 
+def parse_for_email_shifts(mail_content):
+
+    name = None
+    week = None
+    shifts = []
+
+    ptrn_name = re.compile(r'Hello ([\w ]*),')
+    results_name = ptrn_name.findall(mail_content)
+
+    if len(results_name) == 0:
+        print("Unable to read the name")
+        return (None, None, None)
+    # End of if len(result_week)
+
+    name = results_name[0]
+
+    ptrn_week = re.compile(r'week of \**([\d\w, ]+)\**')
+    results_week = ptrn_week.findall(mail_content)
+
+    if len(results_week) == 0:
+        print("Unable to read the week")
+        return (None, None, None)
+    # End of if len(result_week)
+
+    week = results_week[0]
+    
+    ptrn = re.compile(r'\**\w\w\w (\w\w\w) (\d+), (\d\d\d\d)\**((?:[\s -]+\d+:\d\d \w\w - \d+:\d\d \w\w - [\w ]+)+)',re.MULTILINE)
+    results = ptrn.findall(mail_content)
+    
+    if len(results) == 0:
+        print("Unable to read the working days")
+        return (None, None, None)
+    # End of if len(results)
+
+    for result in results:
+        
+        month = result[0]
+        day = result[1]
+        if len(day) == 1:
+            day = '0'+day
+        # End of if len(day)
+        year = result[2]
+        
+        date_str = "%s %s, %s" % (month, day, year)
+        
+        day_content = result[3]
+        
+        ptrn_shift = re.compile(r'(\d)+:(\d\d) (\w\w) - (\d+):(\d\d) (\w\w) - ([\w ]+)',re.MULTILINE)
+        results_shift = ptrn_shift.findall(day_content)
+        
+        # Get the working times in the days working
+        
+        for shift_result in results_shift:
+
+            shift_obj = Shift()
+            shift_obj.day = date_str
+            
+            start_hour = shift_result[0]
+            if len(start_hour) == 1:
+                start_hour = '0'+start_hour
+            # End of if len(star_hour)
+            start_min = shift_result[1]
+            start_am_pm = shift_result[2]
+            
+            end_hour = shift_result[3]
+            if len(end_hour) == 1:
+                end_hour = '0'+end_hour
+            # End of if len(end_hour)
+            end_min = shift_result[4]
+            end_am_pm = shift_result[5]
+            
+            role = shift_result[6]
+            
+            start_time_string = month+' '+day+' '+year+' '+start_hour+' '+start_min+' '+start_am_pm
+            end_time_string = month+' '+day+' '+year+' '+end_hour+' '+end_min+' '+end_am_pm
+            
+            start_date_time_obj = datetime.strptime(start_time_string, '%b %d %Y %I %M %p')
+            end_date_time_obj = datetime.strptime(end_time_string, '%b %d %Y %I %M %p')
+            
+            start_date_time_obj = pytz.timezone('US/Eastern').localize(start_date_time_obj)
+            end_date_time_obj = pytz.timezone('US/Eastern').localize(end_date_time_obj)
+
+            shift_obj.start_date_time_obj = start_date_time_obj
+            shift_obj.end_date_time_obj = end_date_time_obj
+            shift_obj.role = role
+            
+            shifts.append(shift_obj)
+        # End of for shift_result in ...
+    # End of for result in results
+                        
+    return (name, week, shifts)
+# End of def parse_for_email_shifts()
+
+
+
+###########
+# MAIN CODE
+###########
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-c', '--config', type=str, required=True)
+args = parser.parse_args()
+
+config_path = args.config
+
+if os.path.exists(config_path) == False:
+    print("The config path does not exist")
+    sys.exit(1)
+
+config = configparser.ConfigParser()
+
+config.read(config_path)
+
+EMAIL = config['DEFAULT']['EMAIL']
+PASSWORD = config['DEFAULT']['PASSWORD']
+SERVER = config['DEFAULT']['SERVER']
+FILENAME = config['DEFAULT']['FILENAME']
+WAITTIME = int(config['DEFAULT']['WAITTIME'])
 
 while(True):
 
@@ -153,147 +318,60 @@ while(True):
                     mail_content = message.get_payload()
                 # End of if/else message.is_multipart
 
+                mail_content = iphone_fix(mail_content)
+                (name,
+                 week,
+                 shifts_list) = parse_for_email_shifts(mail_content)
 
-                # Handle Iphone forwarding issues
+                if (name is None) or (week is None) or (shifts_list is None):
+                    move_email(mail, i, 'Completed')
+                    continue
+                # End of if (name is None) or ...
 
-                fixed_lines = ""
-                
-                for line in mail_content.split('\n'):
-                    if len(line) == 0:
-                        continue
-                    if line == ">=20\r":
-                        continue
-                    if line[:2] == "> ":
-                        line = line[2:]
-                    
-                    if line[-2:] == "=\r":
-                        fixed_lines+= line[:-2]
-                        continue
-
-                    fixed_lines += line
-                    fixed_lines += '\n'
-
-                # End of for line in mail_content
-                        
-
-                # print(fixed_lines)
-                # print(mail_content)
-
-
-                mail_content = fixed_lines
-            
-                ptrn_week = re.compile(r'week of \**([\d\w, ]+)\**')
-                results_week = ptrn_week.findall(mail_content)
-                ptrn = re.compile(r'\**\w\w\w (\w\w\w) (\d+), (\d\d\d\d)\**((?:[\s -]+\d+:\d\d \w\w - \d+:\d\d \w\w - [\w ]+)+)',re.MULTILINE)
-                results = ptrn.findall(mail_content)
-                
-                if len(results_week) == 0:
-                    print("Unable to read the week")
-                    sys.exit()
-                # End of if len(result_week)
-                
-                week = results_week[0]
-                
-                if len(results) == 0:
-                    print("Unable to read the working days")
-                    sys.exit()
-                # End of if len(results)
-
-                
                 cal = Calendar()
                 dates = []
-                
-                for result in results:
 
-                    month = result[0]
-                    day = result[1]
-                    if len(day) == 1:
-                        day = '0'+day
-                    # End of if len(day)
-                    year = result[2]
-
-                    dates.append("%s %s, %s" % (month, day, year))
-
-                    day_content = result[3]
-
-                    ptrn_shift = re.compile(r'(\d)+:(\d\d) (\w\w) - (\d+):(\d\d) (\w\w) - ([\w ]+)',re.MULTILINE)
-                    results_shift = ptrn_shift.findall(day_content)
+                for shift_obj in shifts_list:
+                    role = shift_obj.role
+                    start_date_time_obj = shift_obj.start_date_time_obj
+                    end_date_time_obj = shift_obj.end_date_time_obj
+                    dates.append(shift_obj.day)
                     
-                    # Get the working times in the days working
-                    
-                    for shift_result in results_shift:
-                    
-                    
-                        start_hour = shift_result[0]
-                        if len(start_hour) == 1:
-                            start_hour = '0'+start_hour
-                        # End of if len(star_hour)
-                        start_min = shift_result[1]
-                        start_am_pm = shift_result[2]
-                        
-                        end_hour = shift_result[3]
-                        if len(end_hour) == 1:
-                            end_hour = '0'+end_hour
-                        # End of if len(end_hour)
-                        end_min = shift_result[4]
-                        end_am_pm = shift_result[5]
-                        
-                        role = shift_result[6]
-                        
-                        
-                    
-                    
-                        start_time_string = month+' '+day+' '+year+' '+start_hour+' '+start_min+' '+start_am_pm
-                        end_time_string = month+' '+day+' '+year+' '+end_hour+' '+end_min+' '+end_am_pm
-
-                        
-                        start_date_time_obj = datetime.strptime(start_time_string, '%b %d %Y %I %M %p')
-                        end_date_time_obj = datetime.strptime(end_time_string, '%b %d %Y %I %M %p')
-                        
-                        start_date_time_obj = pytz.timezone('US/Eastern').localize(start_date_time_obj)
-                        
-                        event = Event()
-                        event.add('summary', role)
-                        event.add('dtstart', start_date_time_obj)
-                        event.add('dtend', end_date_time_obj)
-                        #event.add('description', "Sample Description")
-                        event.add('location', "North Shore Dog, Danvers MA 01923")
-                        cal.add_component(event)
+                    event = Event()
+                    event.add('summary', role)
+                    event.add('dtstart', start_date_time_obj)
+                    event.add('dtend', end_date_time_obj)
+                    #event.add('description', "Sample Description")
+                    event.add('location', "North Shore Dog, Danvers MA 01923")
+                    cal.add_component(event)
                     # End of for shift_result in results_shift
                     
-                # End of for result in results
+                # End of for shift_obj in shifts_list
                 
-                # temp = tempfile.NamedTemporaryFile()
-                filename = r"C:\Users\samjc\Documents\nsdScheduler\nsd_scheduler.ics"
-                
-                #C:\Users\samjc\Documents\nsdScheduler
 
-                f = open(filename, 'wb')
+                f = open(FILENAME, 'wb')
                 f.write(cal.to_ical())
                 f.close()
                 
-                print(mail_from)
+                
                 
                 send_from = EMAIL
                 send_to = [mail_from]
                 subject = "North Shore Dog Calender Invites Week Of %s" % (week)
                 dates_list = "\n".join(dates)
-                text = "Attached are the calender invites for the dates:\n%s" % (dates_list)
-                files = [filename]
-                
-                print(filename)
+                text = "Hello %s,\nAttached are the calender invites for the dates:\n%s" % (name, dates_list)
+                files = [FILENAME]
+
+                print(mail_from)
+                print(name)
+                print(week)
+                value = datetime.fromtimestamp(time.time())
+                print(value.strftime('%Y-%m-%d %H:%M:%S'))
                 
                 send_mail(send_from, send_to, subject, text, files, server="smtp.gmail.com", port=587, username=EMAIL, password=PASSWORD, use_tls=True)
                 
-                # temp.close()
-                
-                result = mail.copy(i, 'Completed')
-                
-                if result[0] == 'OK':
-                    mail.store(i, '+FLAGS', '\\Deleted')
-                    mail.expunge()
+                move_email(mail, i, 'Completed')
                     
-                # End of if result[0]
             # End of if isinstance(response_part, tuple)
         
         # End of for response_part
@@ -303,5 +381,5 @@ while(True):
     mail.close()
 
     if len(mail_ids) == 0:
-        time.sleep(10)
+        time.sleep(WAITTIME)
 # End of while True
